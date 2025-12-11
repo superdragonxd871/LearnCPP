b93f365237b8af94bb8fca2023ce6c184200425f
Sourabh <onlyysourabh871@gmail.com>
2025-12-11 21:24:30 +0530

    learn default construtor

--- practice.cpp
+++ practice.cpp
@@ -3,30 +3,15 @@
 using namespace std;
 
 class product{
-    private:
-    int discount;
     public:
-    string name;
-    int price;
-    int rating;
-
-    void setinfo(string name,int price,int rating,int discount){
-        this-> name=name;
-       this-> price=price;
-       this-> rating=rating;
-       this-> discount=discount;
+    string company;
+     product(){
+        company="apple";
     };
-    void giveinfo(){
-        cout<<name<<endl;
-        cout<<price<<endl;
-        cout<<rating<<endl;
-        cout<<discount<<endl;
-    }
-
 };
 
 int main(){
-    product p1;
-    p1.setinfo("iphone",1000,23,45);
-    p1.giveinfo();
+    product iphone;
+    cout<<iphone.company;
+    
 }
