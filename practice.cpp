631c41a4cc4ecc40b56f960a7f563f59c71f0fd0
Sourabh <onlyysourabh871@gmail.com>
2025-12-13 00:16:06 +0530

    multiple inheritance

--- practice.cpp
+++ practice.cpp
@@ -18,49 +18,42 @@ public:
         cout << age << endl;
     }
 };
-class child : public parent
+class child 
 {
 public:
     string work;
     void setchild(string name, int age, string work)
     {
-        this->name = name;
-        this->age = age;
+        // this->name = name;
+        // this->age = age;
         this->work = work;
     }
     void getchild()
     {
-        cout << name << endl;
-        cout << age << endl;
+        // cout << name << endl;
+        // cout << age << endl;
         cout << work << endl;
     };
 };
 
-class grandchild : public child
+class grandchild : public parent , public child
 {
 public:
-    string address;
-    void setgrand(string name, int age, string work, string address)
+    void setgrand(string name, int age, string work)
     {
         this->name = name;
         this->age = age;
         this->work = work;
-        this->address = address;
     };
-    void getgrand()
-    {
-        cout << name << endl;
-        cout << age << endl;
-        cout << work << endl;
-        cout << address << endl;
-    };
+    void getgrand(){
+        cout<<name<<endl;
+        cout<<age<<endl;
+        cout<<work<<endl;
+    }
 };
 int main()
 {
-    child c1;
-    c1.setchild("sourabh", 21, "student");
-    c1.getchild();
     grandchild g1;
-    g1.setgrand("vikram",20,"grandson","roadparrehtahai");
+    g1.setgrand("sourabh",21,"student");
     g1.getgrand();
 }
